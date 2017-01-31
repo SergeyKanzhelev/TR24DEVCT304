55438558e4bbd12cbde153bf3176273c7a8785dc
Sergey Kanzhelev <SergKanz@microsoft.com>
2017-01-30 16:33:49 -0800

    Initial commit

---
+++ ApplicationInsightsDataROI/Program.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ApplicationInsightsDataROI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var state = new State();
+            state.IsTerminated = false;
+
+            // Create a timer with a two second interval.
+            state._timer = new System.Timers.Timer(TimeSpan.FromSeconds(1).TotalMilliseconds);
+            // Hook up the Elapsed event for the timer. 
+            state._timer.Elapsed += state.OnTimedEvent;
+            state._timer.AutoReset = true;
+            state._timer.Enabled = true;
+
+            TelemetryConfiguration configuration = new TelemetryConfiguration();
+            configuration.InstrumentationKey = "DEMO_KEY";
+
+            var telemetryChannel = new ServerTelemetryChannel();
+            telemetryChannel.Initialize(configuration);
+            configuration.TelemetryChannel = telemetryChannel;
+            
+
+            // data collection modules
+            var dependencies = new DependencyTrackingTelemetryModule();
+            dependencies.Initialize(configuration);
+
+            // telemetry initializers
+            configuration.TelemetryInitializers.Add(new MyTelemetryInitializer());
+
+            // telemetry processors
+            configuration.TelemetryProcessorChainBuilder
+                .Use((next) => { return new PriceCalculatorTelemetryProcessor(next, state.Sent); })
+                .Use((next) => { return new MyTelemetryProcessor(next); })
+                .Use((next) => { return new AdaptiveSamplingTelemetryProcessor(next) {
+                    InitialSamplingPercentage = 10,
+                    SamplingPercentageIncreaseTimeout = TimeSpan.FromSeconds(5),
+                    SamplingPercentageDecreaseTimeout = TimeSpan.FromSeconds(5),
+                    MaxTelemetryItemsPerSecond = 5,
+                    EvaluationInterval = TimeSpan.FromSeconds(10) };})
+                .Use((next) => { return new PriceCalculatorTelemetryProcessor(next, state.Collected); })
+                .Build();
+
+            TelemetryClient client = new TelemetryClient(configuration);
+
+            var iterations = 0;
+
+            while (!state.IsTerminated)
+            {
+                client.TrackEvent("test");
+                iterations++;
+
+                try
+                {
+                    HttpClient http = new HttpClient();
+                    var task = http.GetStringAsync("http://bing.com");
+                    task.Wait();
+
+                    //client.TrackMetric("Response size", task.Result.Length);
+                    //client.TrackMetric("Successful responses", 1);
+                }
+                catch (Exception exc)
+                {
+                    //client.TrackMetric("Successful responses", 0);
+                }
+            }
+
+            Console.WriteLine($"Finished in {iterations} iterations!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ApplicationInsightsDataROI/State.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ApplicationInsightsDataROI
+{
+    class ProcessedItems
+    {
+        public int count;
+        public int size;
+    }
+
+    class State
+    {
+        public System.Timers.Timer _timer;
+
+        public bool IsTerminated { get; set; }
+
+        public ProcessedItems Collected = new ProcessedItems();
+
+        public ProcessedItems Sent = new ProcessedItems();
+
+        public void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (this.IsTerminated)
+            {
+                this._timer.Stop();
+                return;
+            }
+
+            const int oneGb = 1000 * 1000 * 1000;
+            double gbs = 1.0 * this.Collected.size / oneGb;
+
+            if (gbs < 0.1)
+            {
+                Console.WriteLine($"Collected \tsize: {this.Collected.size} bytes \tin {this.Collected.count} items");
+            }
+            else
+            {
+                Console.WriteLine($"Collected \tsize: {gbs} Gb \tin {this.Collected.count} items");
+            }
+
+            gbs = 1.0 * this.Sent.size / oneGb;
+            if (gbs < 0.1)
+            {
+                Console.WriteLine($"Sent    \tsize: {this.Sent.size} bytes \tin {this.Sent.count} items");
+            }
+            else
+            {
+                Console.WriteLine($"Sent    \tsize: {gbs} Gb \tin {this.Sent.count} items");
+            }
+
+            Console.WriteLine();
+
+            if (this.Sent.count > 1000)
+            {
+                this.IsTerminated = true;
+            }
+        }
+
+    }
+}
